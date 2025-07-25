649b7edde8bc40a3cfac7eb177d52f9a9e7176cb
Finot23 <luis.ridr78@gmail.com>
2025-07-25 09:18:22 -0600

    se creo el proyecto

---
+++ src/main/java/com/finalproyect/nodo1/controllers/Nodo1Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.finalproyect.nodo1.controllers;
+
+/**
+ *
+ * @author antoniosalinas
+ */
+public class Nodo1Controller {
+    
+}
---
+++ src/test/java/com/finalproyect/ProyectoFinalApplication/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproyect.ProyectoFinalApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
